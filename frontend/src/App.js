dd4b07155b5689aeca22598169e264ccfa38ac82
Hemraj Jeure <hemraj.jeure22@gmail.com>
2021-02-06 23:24:22 +0530

    Homescreen Componenet Added, Rating Component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import Header from './Components/Header';
+import HomeScreen from './Screens/HomeScreen';
 import Footer from './Components/Footer';
 import { Container } from 'react-bootstrap';
 const App = () => {
@@ -7,7 +8,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome To myShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
---
+++ frontend/src/Components/Product.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Rating from './Rating';
+import { Card } from 'react-bootstrap';
+
+const Product = ({ product }) => {
+  const { _id, image, name, rating, numReviews, price } = product;
+  return (
+    <Card className='my-3 p-3 rounded'>
+      <a href={`/product/${_id}`}>
+        <Card.Img src={image} variant='top'></Card.Img>
+      </a>
+
+      <Card.Body>
+        <a href={`/product/${_id}`}>
+          <Card.Title as='div'>
+            <strong>{name}</strong>
+          </Card.Title>
+        </a>
+
+        <Card.Text as='div'>
+          <Rating value={rating} text={`${numReviews} reviews`} />
+        </Card.Text>
+
+        <Card.Text as='h3'>${price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
