894a7f69743459ea0cad0f7d7d1bfc2c0a1b752d
Hemraj Jeure <hemraj.jeure22@gmail.com>
2021-02-07 21:51:51 +0530

    React Router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
 import Header from './Components/Header';
 import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 import Footer from './Components/Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
--- frontend/src/Components/Product.js
+++ frontend/src/Components/Product.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import Rating from './Rating';
+import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 
 const Product = ({ product }) => {
   const { _id, image, name, rating, numReviews, price } = product;
   return (
     <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${_id}`}>
+      <Link to={`/product/${_id}`}>
         <Card.Img src={image} variant='top'></Card.Img>
-      </a>
+      </Link>
 
       <Card.Body>
-        <a href={`/product/${_id}`}>
+        <Link to={`/product/${_id}`}>
           <Card.Title as='div'>
             <strong>{name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         <Card.Text as='div'>
           <Rating value={rating} text={`${numReviews} reviews`} />
