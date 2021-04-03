43d237cf1b94bff5751fa0d74afae39b9dd5a807
Hemraj Jeure <hemraj.jeure22@gmail.com>
2021-04-03 22:02:35 +0530

    Added profile as private route

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,1 +1,29 @@
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
 
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select('-password');
+      //   console.log(req.user);
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error('Unauthorized, token failed');
+    }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error('Unauthorized, no token');
+  }
+});
+
+export default protect;
