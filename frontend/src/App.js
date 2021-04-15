19c408e81c4d5f5c9ef3896941e472c0175085be
Hemraj Jeure <hemraj.jeure22@gmail.com>
2021-04-15 14:12:22 +0530

    userLogin functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import HomeScreen from './Screens/HomeScreen';
 import ProductScreen from './Screens/ProductScreen';
 import Footer from './Components/Footer';
 import CartScreen from './Screens/CartScreen';
+import LoginScreen from './Screens/LoginScreen';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
@@ -12,9 +13,10 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/' component={HomeScreen} exact />
+          <Route path='/login' component={LoginScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
